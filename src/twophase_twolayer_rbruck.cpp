576c7a37e20a20f029d2bf5a9694c750c9aadaf9
kokofan <koko14fan@gmail.com>
2024-02-27 00:18:10 -0500

    remove timers

--- src/twophase_twolayer_rbruck.cpp
+++ src/twophase_twolayer_rbruck.cpp
@@ -15,7 +15,7 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 									   int *sdispls, MPI_Datatype sendtype, char *recvbuf,
 									   int *recvcounts, int *rdispls, MPI_Datatype recvtype, MPI_Comm comm)
 {
-	double st = MPI_Wtime();
+//	double st = MPI_Wtime();
 	if ( r < 2 ) { return -1; }
 
 	int rank, nprocs;
@@ -44,20 +44,20 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 	max_sd = (ngroup > imax)? ngroup: imax; // max send data block count
 
 	int sent_blocks[max_sd];
-	double et = MPI_Wtime();
-	init_time = et - st;
+//	double et = MPI_Wtime();
+//	init_time = et - st;
 
-	st = MPI_Wtime();
+//	st = MPI_Wtime();
 	// 1. Find max send elements per data-block
 	for (int i = 0; i < nprocs; i++) {
 		if (sendcounts[i] > local_max_count)
 			local_max_count = sendcounts[i];
 	}
 	MPI_Allreduce(&local_max_count, &max_send_count, 1, MPI_INT, MPI_MAX, comm);
-	et = MPI_Wtime();
-	findMax_time = et - st;
+//	et = MPI_Wtime();
+//	findMax_time = et - st;
 
-	st = MPI_Wtime();
+//	st = MPI_Wtime();
 	// 2. create local index array after rotation
 	for (int i = 0; i < ngroup; i++) {
 		int gsp = i*n;
@@ -65,17 +65,17 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 			rotate_index_array[id++] = gsp + (2 * grank - j + n) % n;
 		}
 	}
-	et = MPI_Wtime();
-	rotateIndex_time = et - st;
+//	et = MPI_Wtime();
+//	rotateIndex_time = et - st;
 
-	st = MPI_Wtime();
+//	st = MPI_Wtime();
 	memset(pos_status, 0, nprocs*sizeof(int));
 	memcpy(updated_sentcouts, sendcounts, nprocs*sizeof(int));
 	temp_send_buffer = (char*) malloc(max_send_count*typesize*nprocs);
 	extra_buffer = (char*) malloc(max_send_count*typesize*nprocs);
 	temp_recv_buffer = (char*) malloc(max_send_count*typesize*max_sd);
-	et = MPI_Wtime();
-	alcCopy_time = et - st;
+//	et = MPI_Wtime();
+//	alcCopy_time = et - st;
 
 	// Intra-Bruck
 	getBlock_time = 0, prepData_time = 0, excgMeta_time = 0, excgData_time = 0, replace_time = 0;
@@ -85,7 +85,7 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 			di = 0; spoint = z * distance;
 			if (spoint > n - 1) {break;}
 
-			st = MPI_Wtime();
+//			st = MPI_Wtime();
 			// get the sent data-blocks
 			for (int g = 0; g < ngroup; g++) {
 				for (int i = spoint; i < n; i += next_distance) {
@@ -96,10 +96,10 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 					}
 				}
 			}
-			et = MPI_Wtime();
-			getBlock_time += et - st;
+//			et = MPI_Wtime();
+//			getBlock_time += et - st;
 
-			st = MPI_Wtime();
+//			st = MPI_Wtime();
 			// 2) prepare metadata and send buffer
 			int metadata_send[di];
 			int sendCount = 0, offset = 0;
@@ -117,25 +117,25 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 			int recv_proc = gid*n + (grank + spoint) % n; // receive data from rank + 2^step process
 			int send_proc = gid*n + (grank - spoint + n) % n; // send data from rank - 2^k process
 
-			et = MPI_Wtime();
-			prepData_time += et - st;
+//			et = MPI_Wtime();
+//			prepData_time += et - st;
 
-			st = MPI_Wtime();
+//			st = MPI_Wtime();
 			// 3) exchange metadata
 			int metadata_recv[di];
 			MPI_Sendrecv(metadata_send, di, MPI_INT, send_proc, 0, metadata_recv, di, MPI_INT, recv_proc, 0, comm, MPI_STATUS_IGNORE);
 
 			for(int i = 0; i < di; i++) { sendCount += metadata_recv[i]; }
-			et = MPI_Wtime();
-			excgMeta_time += et - st;
+//			et = MPI_Wtime();
+//			excgMeta_time += et - st;
 
-			st = MPI_Wtime();
+//			st = MPI_Wtime();
 			// 4) exchange data
 			MPI_Sendrecv(temp_send_buffer, offset, MPI_CHAR, send_proc, 1, temp_recv_buffer, sendCount*typesize, MPI_CHAR, recv_proc, 1, comm, MPI_STATUS_IGNORE);
-			et = MPI_Wtime();
-			excgData_time += et - st;
+//			et = MPI_Wtime();
+//			excgData_time += et - st;
 
-			st = MPI_Wtime();
+//			st = MPI_Wtime();
 			// 5) replace
 			offset = 0;
 			for (int i = 0; i < di; i++) {
@@ -147,15 +147,15 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 				pos_status[send_index] = 1;
 				updated_sentcouts[send_index] = metadata_recv[i];
 			}
-			et = MPI_Wtime();
-			replace_time += et - st;
+//			et = MPI_Wtime();
+//			replace_time += et - st;
 
 		}
 		distance *= r;
 		next_distance *= r;
 	}
 
-	st = MPI_Wtime();
+//	st = MPI_Wtime();
 	// organize data
 	int index = 0;
 	for (int i = 0; i < nprocs; i++) {
@@ -168,10 +168,10 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 		}
 		index += d;
 	}
-	et = MPI_Wtime();
-	orgData_time = et - st;
+//	et = MPI_Wtime();
+//	orgData_time = et - st;
 
-	st = MPI_Wtime();
+//	st = MPI_Wtime();
 	int nsend[ngroup], nrecv[ngroup], nsdisp[ngroup], nrdisp[ngroup];
 	int soffset = 0, roffset = 0;
 	for (int i = 0; i < ngroup; i++) {
@@ -185,10 +185,10 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 		nsdisp[i] = soffset, nrdisp[i] = roffset;
 		soffset += nsend[i] * typesize, roffset += nrecv[i] * typesize;
 	}
-	et = MPI_Wtime();
-	prepSP_time = et - st;
+//	et = MPI_Wtime();
+//	prepSP_time = et - st;
 
-	st = MPI_Wtime();
+//	st = MPI_Wtime();
 	MPI_Request* req = (MPI_Request*)malloc(2*ngroup*sizeof(MPI_Request));
 	MPI_Status* stat = (MPI_Status*)malloc(2*ngroup*sizeof(MPI_Status));
 	for (int i = 0; i < ngroup; i++) {
@@ -214,8 +214,8 @@ int twophase_twolayer_rbruck_alltoallv(int n, int r, char *sendbuf, int *sendcou
 	free(temp_send_buffer);
 	free(temp_recv_buffer);
 	free(extra_buffer);
-	et = MPI_Wtime();
-	SP_time = et - st;
+//	et = MPI_Wtime();
+//	SP_time = et - st;
 
 	return 0;
 }
