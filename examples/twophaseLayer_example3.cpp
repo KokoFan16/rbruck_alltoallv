80828050f0b8750db0366a0603cb699a8a75f8e4
kokofan <koko14fan@gmail.com>
2024-04-23 13:55:52 -0400

    update

--- examples/twophaseLayer_example3.cpp
+++ examples/twophaseLayer_example3.cpp
@@ -47,7 +47,7 @@ static void run_rbruckv(int loopcount, int ncores, int nprocs, std::vector<int> 
 	int mpi_errno = MPI_SUCCESS;
 	int basecount = bases.size();
 
-	for (int n = 2; n <= 2048; n = n * 2) {
+	for (int n = 1024; n <= 2048; n = n * 2) {
 
 		int sendcounts[nprocs], sdispls[nprocs], recvcounts[nprocs], rdispls[nprocs];
 		memset(sendcounts, 0, nprocs*sizeof(int));
