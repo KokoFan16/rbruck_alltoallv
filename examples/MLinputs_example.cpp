51c8659bf1b6157c5b1f05e0a0953d4c3c472e11
kokofan <koko14fan@gmail.com>
2024-04-26 12:49:23 -0400

    ML

--- examples/MLinputs_example.cpp
+++ examples/MLinputs_example.cpp
@@ -12,21 +12,22 @@
 int nprocs, rank;
 void readinputs(int rank, std::string filename, std::vector<int> &sendsarray, std::vector<int> &recvcounts);
 std::string getNItem(std::string str, int k, std::string delim, int count);
-int run(int loopcount, int ncores, std::vector<int> bases, int warmup, std::vector<int>& sendsarray, std::vector<int>& recvcounts);
+int run(int loopcount, int ncores, std::vector<int> bases, int warmup, int b, std::vector<int>& sendsarray, std::vector<int>& recvcounts);
 
 int main(int argc, char **argv) {
 
-    if (argc < 5) {
-    	std::cout << "Usage: mpirun -n <nprocs> " << argv[0] << " <inputFile> <loop-count> <ncores-per-node> <base-list> " << std::endl;
+    if (argc < 6) {
+    	std::cout << "Usage: mpirun -n <nprocs> " << argv[0] << " <inputFile> <loop-count> <ncores-per-node> <bblock> <base-list> " << std::endl;
     	return -1;
     }
 
     std::string filename = argv[1];
     int loopCount = atoi(argv[2]);
     int ncores = atoi(argv[3]);
+    int bblock = atoi(argv[4]);
 
     std::vector<int> bases;
-    for (int i = 4; i < argc; i++)
+    for (int i = 5; i < argc; i++)
     	bases.push_back(atoi(argv[i]));
 
     // MPI Initial
@@ -42,21 +43,22 @@ int main(int argc, char **argv) {
 
     readinputs(rank, filename, sendsarray, recvcounts);
 
-    run(10, ncores, bases, 1, sendsarray, recvcounts);
+//    run(10, ncores, bases, 1, sendsarray, recvcounts);
 
-    run(loopCount, ncores, bases, 0, sendsarray, recvcounts);
+    run(loopCount, ncores, bases, 0, bblock, sendsarray, recvcounts);
 
 
     MPI_Finalize();
     return 0;
 }
 
-int run(int loopcount, int ncores, std::vector<int> bases, int warmup, std::vector<int>& sendsarray, std::vector<int>& recvcounts) {
+int run(int loopcount, int ncores, std::vector<int> bases, int warmup, int b, std::vector<int>& sendsarray, std::vector<int>& recvcounts) {
 
 	int basecount = bases.size();
 	int sdispls[nprocs], rdispls[nprocs];
 	long send_tsize = 0, recv_tsize =0;
 	int max_sendn = 0, max_recvn = 0;
+
 	for (int i = 0; i < nprocs; i++) {
 		sdispls[i] = send_tsize;
 		rdispls[i] = recv_tsize;
@@ -88,30 +90,51 @@ int run(int loopcount, int ncores, std::vector<int> bases, int warmup, std::vect
 			double max_time = 0;
 			MPI_Allreduce(&comm_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
 			if (max_time == comm_time)
-				std::cout << "MPI_Alltoallv, " << rank << " " << comm_time << " " << send_tsize << " " <<  recv_tsize << std::endl;
+				std::cout << "MPI_Alltoallv, " << nprocs << " " << comm_time << " " << send_tsize << " " <<  recv_tsize << std::endl;
     	}
     }
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
 
-//	for (int i = 0; i < basecount; i++) {
-//		for (int it=0; it < loopcount; it++) {
-//			double start = MPI_Wtime();
-//			TTPL_BT_alltoallv(ncores, bases[i], (char*)sendbuf, sendsarray.data(), sdispls, MPI_CHAR, (char*)recvbuf, recvcounts.data(), rdispls, MPI_CHAR, MPI_COMM_WORLD);
-//			double end = MPI_Wtime();
-//			double comm_time = (end - start);
-//
-//			if (warmup == 0) {
-//				double max_time = 0;
-//				MPI_Allreduce(&comm_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-//				if (max_time == comm_time)
-//					std::cout << "TwoPhaseLayer, " << rank << " " << comm_time << " " << send_tsize << " " <<  recv_tsize << std::endl;
-//			}
-//		}
-//	}
-//
-//	MPI_Barrier(MPI_COMM_WORLD);
+	for (int i = 0; i < basecount; i++) {
+		for (int it=0; it < loopcount; it++) {
+
+			double start = MPI_Wtime();
+			TTPL_BT_alltoallv(ncores, bases[i], b, (char*)sendbuf, sendsarray.data(), sdispls, MPI_CHAR, (char*)recvbuf, recvcounts.data(), rdispls, MPI_CHAR, MPI_COMM_WORLD);
+			double end = MPI_Wtime();
+			double comm_time = (end - start);
+
+			if (warmup == 0) {
+				double max_time = 0;
+				MPI_Allreduce(&comm_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+				if (max_time == comm_time)
+					std::cout << "LTRNA_S1, " << nprocs << " " << b << " " << bases[i] << ""  << comm_time << " " << send_tsize << " " <<  recv_tsize << std::endl;
+			}
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+
+	for (int i = 0; i < basecount; i++) {
+		for (int it=0; it < loopcount; it++) {
+
+			double start = MPI_Wtime();
+			TTPL_BT_alltoallv_s1(ncores, bases[i], b, (char*)sendbuf, sendsarray.data(), sdispls, MPI_CHAR, (char*)recvbuf, recvcounts.data(), rdispls, MPI_CHAR, MPI_COMM_WORLD);
+			double end = MPI_Wtime();
+			double comm_time = (end - start);
+
+			if (warmup == 0) {
+				double max_time = 0;
+				MPI_Allreduce(&comm_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+				if (max_time == comm_time)
+					std::cout << "LTRNA_S2, " << nprocs << " " << b << " " << bases[i] << ""  << comm_time << " " << send_tsize << " " <<  recv_tsize << std::endl;
+			}
+		}
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
 
     free(sendbuf);
     free(recvbuf);
