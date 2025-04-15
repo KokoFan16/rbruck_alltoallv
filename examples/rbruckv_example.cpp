50ff037cf852325685c0b501e6e5965365aa4f91
kokofan <koko14fan@gmail.com>
2025-04-14 21:38:40 -0500

    debug

--- examples/rbruckv_example.cpp
+++ examples/rbruckv_example.cpp
@@ -46,7 +46,7 @@ static void run_rbruckv(int loopcount, int nprocs, std::vector<int> bases, int w
 
 	int mpi_errno = MPI_SUCCESS;
 	int basecount = bases.size();
-	for (int n = 512; n <= 512; n = n * 2) {
+	for (int n = 2; n <= 2048; n = n * 2) {
 
 		int sendcounts[nprocs]; // the size of data each process send to other process
 		memset(sendcounts, 0, nprocs*sizeof(int));
